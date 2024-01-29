81ca56e6fbf67dd94ae0a7c0ed1304c14792b671
Altynaim6 <altynaimunduzbaeva@gmail.com>
2024-01-29 10:24:48 +0600

    modify code

--- Main.java
+++ Main.java
@@ -448,4 +448,8 @@ public class Main {
                 }
             }
         }
+        public static void clearScreen() {
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
+        }
 }
