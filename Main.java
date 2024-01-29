87eba871a7c55c9e9113879b06bd1389d3eb6067
Altynaim6 <altynaimunduzbaeva@gmail.com>
2024-01-29 10:15:26 +0600

    place ship

--- Main.java
+++ Main.java
@@ -20,6 +20,105 @@ public class Main {
         System.out.println("7. The game ends when all enemy ships are sunk.");
         System.out.println("Good luck! Let the battle begin!");
 
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 7; j++) {
+                field[i][j] = 0;
+            }
+        }
+
+        placeShip(field, 3);
+        placeShip(field, 2);
+        placeShip(field, 4);
+        placeShip(field, 1);
+        placeShip(field, 1);
+        placeShip(field, 1);
+        placeShip(field, 1);
+
+    }
+    public static void placeShip(Integer[][] field, int shipSize) {
+        Random random = new Random();
+        boolean placed = false;
+
+        while (!placed) {
+            int row = random.nextInt(7);
+            int col = random.nextInt(7);
+            boolean isVertical = random.nextBoolean();
+
+            if (isValidPosition(field, row, col, shipSize, isVertical)) {
+                if(shipSize == 1) {
+                    for (int i = 0; i < shipSize; i++) {
+                        if (isVertical) {
+                            field[row + i][col] = 1;
+                        } else {
+                            field[row][col + i] = 1;
+                        }
+                    }
+                } else if (shipSize == 2) {
+                    for (int i = 0; i < shipSize; i++) {
+                        if (isVertical) {
+                            field[row + i][col] = 2;
+                        } else {
+                            field[row][col + i] = 2;
+                        }
+                    }
+                } else if (shipSize == 4) {
+                    for (int i = 0; i < shipSize - 2; i++) {
+                        if (isVertical) {
+                            field[row + i][col] = 4;
+                        } else {
+                            field[row][col + i] = 4;
+                        }
+                    }
+                } else if (shipSize == 3) {
+                    for (int i = 0; i < shipSize; i++) {
+                        if (isVertical) {
+                            field[row + i][col] = 3;
+                        } else {
+                            field[row][col + i] = 3;
+                        }
+                    }
+                }
+                placed = true;
+            }
+        }
     }
 
+    public static boolean isValidPosition(Integer[][] field, int row, int col, int shipSize, boolean isVertical) {
+        if (isVertical && row + shipSize > 7) {
+            return false;
+        }
+        if (!isVertical && col + shipSize > 7) {
+            return false;
+        }
+
+        for (int i = 0; i < shipSize; i++) {
+            if (isVertical) {
+                if (field[row + i][col] != 0 || hasAdjacentShip(field, row + i, col)) {
+                    return false;
+                }
+            } else {
+                if (field[row][col + i] != 0 || hasAdjacentShip(field, row, col + i)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean hasAdjacentShip(Integer[][] field, int row, int col) {
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                int newRow = row + i;
+                int newCol = col + j;
+
+                if (newRow >= 0 && newRow < 7 && newCol >= 0 && newCol < 7) {
+                    if (field[newRow][newCol] != 0 && field[newRow][newCol] != 0) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
 }
